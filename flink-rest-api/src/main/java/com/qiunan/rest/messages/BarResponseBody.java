98f04211480a3360d5b7f51d0ab6fbd41c8f27ad
qiunan <v-qiuna@oppo.com>
2022-02-21 18:51:07 +0800

    自定义 rest 接口

---
+++ flink-rest-api/src/main/java/com/qiunan/rest/messages/BarResponseBody.java
@@ -0,0 +1,17 @@
+package com.qiunan.rest.messages;
+
+import org.apache.flink.runtime.rest.messages.ResponseBody;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.annotation.JsonProperty;
+
+public class BarResponseBody implements ResponseBody {
+    private static final String FIELD_BAR = "response";
+
+    @JsonProperty(FIELD_BAR)
+    public final String response = "bar";
+
+    private static final BarResponseBody INSTANCE = new BarResponseBody();
+
+    public static BarResponseBody getInstance() {
+        return INSTANCE;
+    }
+}
